a8139ca51d2110f6b8308be91b55b6872850fdb6
lombardoh <lombardo.h@hotmail.com>
2023-05-12 19:05:47 -0300

    added test_requeriments.py

---
+++ tmp/test_requeriments.py
@@ -0,0 +1,10 @@
+import pytest
+import requests
+
+required_packages = [
+    "fastapi>=0.95.1",
+    "pydantic>=1.10.7",
+    "python>dotenv==1.0.0",
+    "SQLAlchemy>=1.4.41",
+    "sqlmodel>=0.0.8",
+]
