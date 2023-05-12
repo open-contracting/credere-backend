205b89bbdf7bfa7aba02cbebcc1e8fe013b4126c
lombardoh <lombardo.h@hotmail.com>
2023-05-12 17:23:33 -0300

    removed unused import

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-import httpx
 from fastapi.testclient import TestClient
 
 from app.core.settings import Settings
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,3 @@
-import httpx
 from fastapi.testclient import TestClient
 
 from app.main import app
