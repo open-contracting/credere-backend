fd59760908c388112d2e978bf68dd603cc86aa41
lombardoh <lombardo.h@hotmail.com>
2023-05-15 12:46:43 -0300

    added a placeholder use for httpx, I have nothing to test with it right now and its required for Lint

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from functools import lru_cache
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
-
 from .core.settings import Settings
 from .routers import users
 
@@ -20,6 +19,6 @@ def read_root():
     return {"Title": "Credence backend"}
 
 
-@app.get("/info")
+@app.api_route("/info")
 async def info(settings: Annotated[Settings, Depends(get_settings)]):
     return {"Title": "Credence backend", "version": settings.version}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-
+import httpx
 from app.core.settings import Settings
 from app.main import app
 
@@ -11,3 +11,8 @@ def test_info_endpoint():
     response = client.get("/info")
     assert response.status_code == 200
     assert response.json() == {"Title": "Credence backend", "version": settings.version}
+
+
+def test_info_endpoint():
+    response = httpx.get("https://www.google.com/")
+    assert response.status_code == 200
