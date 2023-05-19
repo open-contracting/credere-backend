baf1604e309e99a5e4a217fac3c4f23243adb717
lombardoh <lombardo.h@hotmail.com>
2023-05-19 13:15:12 -0300

    added default region for aws cognito tests

--- app/core/settings.py
+++ app/core/settings.py
@@ -17,7 +17,7 @@ config_env = {
 class Settings(BaseSettings):
     app_name: str = "Credere API"
     version: str = config_env.get("VERSION", "0.1.1")
-    cognito_aws_region: str = config_env.get("COGNITO_AWS_REGION", None)
+    cognito_aws_region: str = config_env.get("COGNITO_AWS_REGION", "us-east-1")
     cognito_pool_id: str = config_env.get("COGNITO_POOL_ID", None)
     cognito_client_id: str = config_env.get("COGNITO_CLIENT_ID", None)
     cognito_secret_key: str = config_env.get("COGNITO_SECRET_KEY", None)
