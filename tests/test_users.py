fccab75df643aed884e61991b1f62f0c6c09e9dc
lombardoh <lombardo.h@hotmail.com>
2023-05-19 11:46:18 -0300

    re compiled req files and changed region in test from a variable to a fixed value

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,10 +4,9 @@ from moto import mock_cognitoidp
 
 @mock_cognitoidp
 def test_cognito_authorization_process():
-    region = "us-east-1"
     password = "SecurePassword1234#$%"
     username = "test.user@willdom.com"
-    cognito_client = boto3.client("cognito-idp", region_name=region)
+    cognito_client = boto3.client("cognito-idp", region_name="us-east-1")
     user_pool_id = cognito_client.create_user_pool(PoolName="TestUserPool")["UserPool"]["Id"]
     cognito_client.create_user_pool_client(UserPoolId=user_pool_id, ClientName="TestAppClient")
 
