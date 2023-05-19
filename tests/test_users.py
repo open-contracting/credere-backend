fd14db2ff4e9ecd7adf9cc379f53e936b1b5952b
lombardoh <lombardo.h@hotmail.com>
2023-05-19 12:00:13 -0300

    added default region in test to fix no region error

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,15 @@
 import boto3
+from botocore.config import Config
 from moto import mock_cognitoidp
+
+my_config = Config(region_name="us-east-1")
 
 
 @mock_cognitoidp
 def test_cognito_authorization_process():
     password = "SecurePassword1234#$%"
     username = "test.user@willdom.com"
-    cognito_client = boto3.client("cognito-idp", region_name="us-east-1")
+    cognito_client = boto3.client("cognito-idp", config=my_config)
     user_pool_id = cognito_client.create_user_pool(PoolName="TestUserPool")["UserPool"]["Id"]
     cognito_client.create_user_pool_client(UserPoolId=user_pool_id, ClientName="TestAppClient")
 
