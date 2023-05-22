90be3d5f5f0e114e5c4757a51e19da88d36eb3fa
lombardoh <lombardo.h@hotmail.com>
2023-05-22 10:53:11 -0300

    added application actions model to user endpoint, everything is a placeholder for display

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,19 @@
+from datetime import datetime
+
 from fastapi.testclient import TestClient
 
 from app.main import app
-from app.schema.user_tables.users import User
+from app.schema.user_tables.users import ApplicationAction, User
+
+test_time = datetime.now()
+
+application_action = ApplicationAction(
+    type="action_type",
+    data={"key1": "value1", "key2": "value2"},
+    application_id="application_id_value",
+    user_id=123,
+    created_at=test_time,
+)
 
 user = User(
     id=1,
@@ -16,5 +28,10 @@ def test_read_user():
     client = TestClient(app)
     response = client.get("/users/")
 
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == user
+    assert response_json["id"] == user.id
+    assert response_json["type"] == user.type
+    assert response_json["email"] == user.email
+    assert response_json["external_id"] == user.external_id
+    assert response_json["fl_id"] == user.fl_id
