d0c866f17058ba98517f328b18e952f884f0bc8d
Nahuel Hernández <jnahuelhernandez@gmail.com>
2023-05-25 15:24:13 -0400

    feat: CORS support

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .core.settings import Settings
 from .routers import users
 
 app = FastAPI()
+
+# Configure CORS settings
+origins = [
+    Settings().frontend_url
+    # Add more allowed origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
 
 
