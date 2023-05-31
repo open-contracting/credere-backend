1cd1caf3626939ef8374c4de9f0daddc4c3160fe
adrian <adrian.martinez@willdom.com>
2023-05-31 18:52:46 -0400

    added remove me and findout more links

--- app/email_templates/__init__.py
+++ app/email_templates/__init__.py
@@ -1,8 +1,11 @@
 NEW_USER_TEMPLATE_NAME = "credere-NewAccountCreated"
-FI_New_application_submission_FI_user = "credere-newApplicationSubmissionFIuser"
-New_Account_Created = "credere-OverdueApplicationOCPadmin"
-New_application_submission_OCP_user = "credere-newApplicationSubmissionOCPUser"
-Overdue_application_FI = "credere-OverdueApplicationFI"
-Overdue_application_OCP_admin = "credere-OverdueApplicationOCPadmin"
-Reset_password = "credere-ResetPassword"
-Access_to_credit_scheme_for_MSMEs = "credere-AccessToCreditSchemeForMSMEs"
+FI_NEW_APPLICATION_SUBMISSION_FI_USER_TEMPLATE_NAME = (
+    "credere-newApplicationSubmissionFIuser"
+)
+NEW_APPLICATION_SUBMISSION_OCP_USER_TEMPLATE_NAME = (
+    "credere-newApplicationSubmissionOCPUser"
+)
+OVERDUE_APPLICATION_FI_TEMPLATE_NAME = "credere-OverdueApplicationFI"
+OVERDUE_APPLICATION_OCP_ADMIN_TEMPLATE_NAME = "credere-OverdueApplicationOCPadmin"
+RESET_PASSWORD_TEMPLATE_NAME = "credere-ResetPassword"
+ACCESS_TO_CREDIT_SCHEME_FOR_MSMES_TEMPLATE_NAME = "credere-AccessToCreditSchemeForMSMEs"
