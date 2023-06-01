55c30e4f84f5638f3f952b3de8f9d5f9a2a5965f
adrian <adrian.martinez@willdom.com>
2023-06-01 18:36:41 -0400

    added default value to temporal bucket

--- app/core/settings.py
+++ app/core/settings.py
@@ -26,9 +26,15 @@ class Settings(BaseSettings):
     database_url: str = config_env.get("DATABASE_URL", None)
     frontend_url: str = config_env.get("FRONTEND_URL", "http://localhost:3000")
     sentry_dsn: str = config_env.get("SENTRY_DNS", None)
-    temporal_bucket: str = config_env.get("TEMPORAL_BUCKET", None)
-    front_public_images_es: str = config_env.get("FRONT_PUBLIC_IMAGES_ES", None)
-    front_public_images_en: str = config_env.get("FRONT_PUBLIC_IMAGES_EN", None)
+    temporal_bucket: str = config_env.get(
+        "TEMPORAL_BUCKET", "https://adrian-personal.s3.sa-east-1.amazonaws.com"
+    )
+    front_public_images_es: str = config_env.get(
+        "FRONT_PUBLIC_IMAGES_ES", "/public/images/es"
+    )
+    front_public_images_en: str = config_env.get(
+        "FRONT_PUBLIC_IMAGES_EN", "/public/images/es"
+    )
 
     class Config:
         env_file = ".env"
