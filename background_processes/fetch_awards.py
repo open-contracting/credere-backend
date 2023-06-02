112e3e78e9ec7b16482b8b9d4f39e74deb150815
lombardoh <lombardo.h@hotmail.com>
2023-06-02 17:29:53 -0300

    remoted __init__.py from background_processes to avoid it being recognized as module

--- background_processes/fetch_awards.py
+++ background_processes/fetch_awards.py
@@ -4,13 +4,12 @@ from application_utils import create_application
 
 
 create_application(1, 1, "asd", "asd", "asd")
-# contracts_response = get_new_contracts()
-# contracts_response_json = contracts_response.json()
+contracts_response = get_new_contracts()
+contracts_response_json = contracts_response.json()
 
-# if not contracts_response_json:
-#     print("No new contracts")
-# else:
-#     for entry in contracts_response_json:
-#         borrower_id = get_or_create_borrower(entry)
-#         fetched_award = create_award(entry, borrower_id)
-#         break
+if not contracts_response_json:
+    print("No new contracts")
+else:
+    for entry in contracts_response_json:
+        borrower_id = get_or_create_borrower(entry)
+        fetched_award = create_award(entry, borrower_id)
