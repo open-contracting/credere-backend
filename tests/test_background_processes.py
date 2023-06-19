9a1dd89ad71b176c3c70ebc455d0fd399a99dcdd
lombardoh <lombardo.h@hotmail.com>
2023-06-19 10:21:40 -0300

    feat: added first batch of test for background processes

--- tests/test_background_processes.py
+++ tests/test_background_processes.py
@@ -1,176 +1,160 @@
-import logging
-import unittest
-from typing import Any, Generator
-from unittest.mock import MagicMock, patch
-
-import boto3
 import pytest
-from botocore.config import Config
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-from moto import mock_cognitoidp, mock_ses
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from app.background_processes import fetcher
-from app.core.settings import app_settings
-from app.core.user_dependencies import CognitoClient, get_cognito_client
-from app.db.session import get_db
-from app.email_templates import ACCESS_TO_CREDIT_SCHEME_FOR_MSMES_TEMPLATE_NAME
-from app.schema.core import Borrower
-from app.utils.email_utility import send_invitation_email
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from app.schema.core import Award, Borrower
+from app.background_processes import awards_utils
+from app.background_processes.borrower_utils import get_or_create_borrower
+from sqlmodel import create_engine
+from unittest.mock import MagicMock, patch
+import os
+import json
 
 
-def start_application():
-    app = FastAPI()
-    return app
+class MockResponse:
+    def __init__(self, status_code, json_data):
+        self.status_code = status_code
+        self.json_data = json_data
+
+    def json(self):
+        return self.json_data
+
+
+def load_json_file(filename):
+    # Get the directory path of the current file
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the full path to the JSON file
+    filepath = os.path.join(current_dir, filename)
+
+    # Open the JSON file and read its contents
+    with open(filepath, "r") as json_file:
+        data = json.load(json_file)
+
+    return data
+
+
+# Load the contract data
+contract = load_json_file("contract.json")
+
+# Load the award data
+award = load_json_file("award.json")
+
+# Load the borrower data
+borrower = load_json_file("borrower.json")
 
 
 @pytest.fixture(scope="function")
-def app() -> Generator[FastAPI, Any, None]:
-    logging.info("Creating test database")
-    Borrower.metadata.create_all(engine)  # Create the tables.
-    _app = start_application()
-    yield _app
-    Borrower.metadata.drop_all(engine)
+def db_session():
+    # Create a database session
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = SessionTesting()
 
+    # Create tables
+    Award.metadata.create_all(engine)
 
-@pytest.fixture(autouse=True)
-def mock_cognito_client():
-    with mock_cognitoidp():
-        yield
+    # Yield the session to the test case
+    yield session
 
-
-@pytest.fixture(autouse=True)
-def mock_ses_client():
-    with mock_ses():
-        yield
+    # Drop tables and close the session
+    session.close()
+    Award.metadata.drop_all(engine)
 
 
 @pytest.fixture(scope="function")
-def client(app: FastAPI) -> Generator[TestClient, Any, None]:
-    my_config = Config(region_name=app_settings.aws_region)
+def mock_get_email():
+    # Create a mock for make_request_with_retry
+    mock = MagicMock(return_value=MockResponse(200, "test_email@test.com"))
 
-    ses_client = boto3.client("ses", config=my_config)
+    # Patch the make_request_with_retry function
+    with patch(
+        "app.background_processes.colombia_data_access.get_email",
+        mock,
+    ):
+        yield mock
 
-    app_settings.cognito_client_secret = "secret"
 
-    ses_client.verify_email_identity(EmailAddress=app_settings.email_sender_address)
+@pytest.fixture(scope="function")
+def mock_request_award():
+    # Create a mock for make_request_with_retry
+    mock = MagicMock(return_value=MockResponse(200, award))
 
-    ses_client.create_template(
-        Template={
-            "TemplateName": ACCESS_TO_CREDIT_SCHEME_FOR_MSMES_TEMPLATE_NAME,
-            "SubjectPart": "Your email subject",
-            "HtmlPart": "<html><body>Your HTML content</body></html>",
-            "TextPart": "Your plain text content",
-        }
+    # Patch the make_request_with_retry function
+    with patch(
+        "app.background_processes.background_utils.make_request_with_retry",
+        mock,
+    ):
+        yield mock
+
+
+@pytest.fixture(scope="function")
+def mock_request_borrower():
+    # Create a mock for make_request_with_retry
+    mock = MagicMock(return_value=MockResponse(200, borrower))
+
+    # Patch the make_request_with_retry function
+    with patch(
+        "app.background_processes.background_utils.make_request_with_retry",
+        mock,
+    ):
+        yield mock
+
+
+@pytest.fixture(scope="function")
+def mock_empty_request():
+    # Create a mock for make_request_with_retry
+    mock = MagicMock(return_value=MockResponse(200, []))
+
+    # Patch the make_request_with_retry function
+    with patch(
+        "app.background_processes.background_utils.make_request_with_retry",
+        mock,
+    ):
+        yield mock
+
+
+def test_create_award(db_session, mock_request_award):
+    # Call the function under test
+    result = awards_utils.create_award(contract, db_session)
+    inserted_award = db_session.query(Award).first()
+
+    # Assert the result
+    assert result == inserted_award
+
+
+def test_create_duplicate_award(db_session, mock_request_award):
+    with pytest.raises(ValueError) as cm:
+        awards_utils.create_award(contract, db_session)
+        awards_utils.create_award(contract, db_session)
+
+    # Assert the error message
+    assert (
+        str(cm.value) == "Skipping Award [previous False] - Already exists on database"
     )
 
-    def _get_test_cognito_client():
-        try:
-            yield CognitoClient(
-                ses_client,
-            )
-        finally:
-            pass
 
-    connection = engine.connect()
-    session = SessionTesting(bind=connection)
+def test_create_award_empty_response(db_session, mock_empty_request):
+    with pytest.raises(ValueError) as cm:
+        awards_utils.create_award(contract, db_session)
 
-    def _get_test_db():
-        try:
-            yield session
-        finally:
-            session.close()
-
-    # Override the clients dependencies with the mock implementations
-    app.dependency_overrides[get_cognito_client] = _get_test_cognito_client
-    app.dependency_overrides[get_db] = _get_test_db
-
-    with TestClient(app) as client:
-        yield client
-        connection.close()
-
-
-def test_send_invitation_email():
-    ses_client = client
-
-    borrower_name = "John Doe"
-    buyer_name = "ABC Company"
-    tender_title = "Sample Tender"
-    uuid = "abcd-1234"
-    email = "test@example.com"
-
-    mock_send_templated_email = MagicMock(
-        return_value={"MessageId": "mocked_message_id"}
+    # Assert the error message
+    assert (
+        str(cm.value) == "Skipping Award [previous False]"
+        " - Zero or more than one results for 'proceso_de_compra' and 'proveedor_adjudicado'",
     )
 
-    ses_client.send_templated_email = mock_send_templated_email
 
-    send_invitation_email(
-        ses_client, uuid, email, borrower_name, buyer_name, tender_title
-    )
+def test_create_award_no_source_contract_id(db_session, mock_request_award):
+    # remove id_contrato so it raises an error
+    contract.pop("id_contrato")
+    with pytest.raises(ValueError) as cm:
+        awards_utils.create_award(contract, db_session)
 
-    mock_send_templated_email.assert_called_once()
-
-
-# mover a json y completar todos los campos
-initial_response = {
-    "contract1": {
-        "nombre_entidad": "One Entity",
-        "nit_entidad": "890205176",
-        "departamento": "One Department",
-        "ciudad": "One City",
-    },
-    "contract2": {
-        "nombre_entidad": "Another Entity",
-        "nit_entidad": "123456789",
-        "departamento": "Some Department",
-        "ciudad": "Some City",
-    },
-}
-
-modified_response = []
-
-mock_response = MagicMock()
-mock_response.status_code = 200
-mock_response.json.side_effect = [initial_response, modified_response]
-
-mock_get_new_contracts = MagicMock(return_value=mock_response)
+    # Assert the error message
+    assert (str(cm.value) == "ValueError: Skipping Award - No id_contrato",)
 
 
-class FetchNewAwardsTestCase(unittest.TestCase):
-    def test_fetch_new_awards(self):
-        mock_get_new_contracts = MagicMock(return_value=mock_response)
-
-        with patch(
-            "app.background_processes.awards_utils.get_new_contracts",
-            mock_get_new_contracts,
-        ):
-            fetcher.fetch_new_awards()
-
-
-class FetchPreviousAwardsTestCase(unittest.TestCase):
-    def test_fetch_previous_awards(self):
-        borrower = Borrower(
-            legal_identifier="123456789",
-            email="test@example.com",
-        )
-
-        mock_response = MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.side_effect = [initial_response, modified_response]
-
-        mock_get_previous_contracts = MagicMock(return_value=mock_response)
-
-        with patch(
-            "app.background_processes.awards_utils.get_previous_contracts",
-            mock_get_previous_contracts,
-        ):
-            fetcher.fetch_previous_awards(borrower)
+def test_create_borrower(db_session, mock_request_borrower, mock_get_email):
+    result = get_or_create_borrower(contract, db_session)
+    inserted_borrower = db_session.query(Borrower).first()
+    assert result == inserted_borrower
