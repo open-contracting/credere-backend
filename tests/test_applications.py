8606cc6ef936d62f5c22ab318a90306be498ce9d
lombardoh <lombardo.h@hotmail.com>
2023-07-02 18:05:43 -0300

    feat: completed tests for lenders and apps

---
+++ tests/test_applications.py
@@ -0,0 +1,127 @@
+import logging
+
+from fastapi import status
+
+import tests.test_client as test_client
+from app.schema.core import ApplicationStatus, BorrowerStatus
+
+from tests.test_client import app, client  # isort:skip # noqa
+from tests.test_client import mock_cognito_client, mock_ses_client  # isort:skip # noqa
+
+
+def test_get_applications(client):  # isort:skip # noqa
+    logging.info("Pre load users for this test")
+    OCP_headers = test_client.create_test_user(client, test_client.OCP_user)
+    FI_headers = test_client.create_test_user(client, test_client.FI_user)
+    logging.info(
+        "Pre load an application with its related award and borrower for this test"
+    )
+    test_client.create_application()
+
+    logging.info("Test different get methods and permissions for getting applications")
+    response = client.get(
+        "/applications/admin-list/?page=1&page_size=4&sort_field=borrower.legal_name&sort_order=asc",
+        headers=OCP_headers,
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+    response = client.get(
+        "/applications/admin-list/?page=1&page_size=4&sort_field=borrower.legal_name&sort_order=asc"
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    response = client.get("/applications/id/1", headers=OCP_headers)
+    assert response.status_code == status.HTTP_200_OK
+
+    response = client.get("/applications/id/1")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    logging.info("get only applications related to FI user")
+    response = client.get("/applications", headers=FI_headers)
+    assert response.status_code == status.HTTP_200_OK
+
+    response = client.get("/applications")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    response = client.get("/applications/uuid/123-456-789")
+    assert response.status_code == status.HTTP_200_OK
+
+    logging.info("set application to expire so use cannot get it")
+    test_client.set_application_as_expired()
+    response = client.get("/applications/uuid/123-456-789")
+    assert response.status_code == status.HTTP_409_CONFLICT
+
+    response = client.get("/applications/uuid/123-456")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    logging.info("Search for a non existent application")
+    response = client.post("/applications/access-scheme", json={"uuid": "123-456"})
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_access_scheme(client):  # isort:skip # noqa
+    test_client.create_application()
+
+    response = client.post("/applications/access-scheme", json={"uuid": "123-456-789"})
+    assert response.json()["application"]["status"] == ApplicationStatus.ACCEPTED.value
+    assert response.status_code == status.HTTP_200_OK
+
+    logging.info("Application should be accepted now so it cannot be accepted again")
+    response = client.post("/applications/access-scheme", json={"uuid": "123-456-789"})
+    assert response.status_code == status.HTTP_409_CONFLICT
+
+
+def test_application_declined(client):  # isort:skip # noqa
+    test_client.create_application()
+
+    response = client.post(
+        "/applications/decline",
+        json={"uuid": "123-456-789", "decline_this": False, "decline_all": True},
+    )
+    assert response.json()["application"]["status"] == ApplicationStatus.DECLINED.value
+    assert (
+        response.json()["borrower"]["status"]
+        == BorrowerStatus.DECLINE_OPPORTUNITIES.value
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+    logging.info("Application should be accepted now because it was declined")
+    response = client.post("/applications/access-scheme", json={"uuid": "123-456-789"})
+    assert response.status_code == status.HTTP_409_CONFLICT
+
+
+def test_application_rollback_declined(client):  # isort:skip # noqa
+    test_client.create_application(ApplicationStatus.DECLINED)
+    test_client.set_borrower_status(BorrowerStatus.DECLINE_OPPORTUNITIES)
+
+    response = client.post(
+        "/applications/rollback-decline", json={"uuid": "123-456-789"}
+    )
+
+    assert response.json()["application"]["status"] == ApplicationStatus.PENDING.value
+    assert response.json()["borrower"]["status"] == BorrowerStatus.ACTIVE.value
+    assert response.status_code == status.HTTP_200_OK
+
+    logging.info("Application is not rejected now so it cannot be rolled back again")
+    response = client.post(
+        "/applications/rollback-decline", json={"uuid": "123-456-789"}
+    )
+    assert response.status_code == status.HTTP_409_CONFLICT
+
+
+def test_application_declined_feedback(client):  # isort:skip # noqa
+    test_client.create_application(ApplicationStatus.DECLINED)
+
+    declined_feedback = {
+        "uuid": "123-456-789",
+        "dont_need_access_credit": True,
+        "already_have_acredit": False,
+        "preffer_to_go_to_bank": False,
+        "dont_want_access_credit": False,
+        "other": False,
+        "other_comments": "comments",
+    }
+
+    response = client.post("/applications/decline-feedback", json=declined_feedback)
+    assert response.json()["application"]["status"] == ApplicationStatus.DECLINED.value
+    assert response.status_code == status.HTTP_200_OK
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,135 +1,17 @@
 import logging
-from typing import Any, Generator
 
-import boto3
-import pytest
-from botocore.config import Config
-from fastapi import FastAPI, status
-from fastapi.testclient import TestClient
-from moto import mock_cognitoidp, mock_ses
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from fastapi import status
 
-from app.core.settings import app_settings
-from app.core.user_dependencies import CognitoClient, get_cognito_client
-from app.db.session import get_db
-from app.email_templates import NEW_USER_TEMPLATE_NAME
-from app.routers import security, users
-from app.schema.core import User, UserType
+import tests.test_client as test_client
+from app.schema.core import UserType
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(name)s - %(message)s",
-    handlers=[logging.StreamHandler()],  # Output logs to the console
-)
-
-
-def start_application():
-    app = FastAPI()
-    app.include_router(users.router)
-    app.include_router(security.router)
-    return app
-
-
-@pytest.fixture(scope="function")
-def app() -> Generator[FastAPI, Any, None]:
-    logging.info("Creating test database")
-    User.metadata.create_all(engine)  # Create the tables.
-    _app = start_application()
-    yield _app
-    User.metadata.drop_all(engine)
-
-
-@pytest.fixture(autouse=True)
-def mock_cognito_client():
-    with mock_cognitoidp():
-        yield
-
-
-@pytest.fixture(autouse=True)
-def mock_ses_client():
-    with mock_ses():
-        yield
-
-
-tempPassword = "1234567890Abc!!"
-
-
-@pytest.fixture(scope="function")
-def client(app: FastAPI) -> Generator[TestClient, Any, None]:
-    my_config = Config(region_name=app_settings.aws_region)
-
-    cognito_client = boto3.client("cognito-idp", config=my_config)
-    ses_client = boto3.client("ses", config=my_config)
-
-    cognito_pool_id = cognito_client.create_user_pool(PoolName="TestUserPool")[
-        "UserPool"
-    ]["Id"]
-
-    app_settings.cognito_pool_id = cognito_pool_id
-
-    cognito_client_name = "TestAppClient"
-
-    cognito_client_id = cognito_client.create_user_pool_client(
-        UserPoolId=cognito_pool_id, ClientName=cognito_client_name
-    )["UserPoolClient"]["ClientId"]
-
-    app_settings.cognito_client_id = cognito_client_id
-    app_settings.cognito_client_secret = "secret"
-
-    ses_client.verify_email_identity(EmailAddress=app_settings.email_sender_address)
-
-    ses_client.create_template(
-        Template={
-            "TemplateName": NEW_USER_TEMPLATE_NAME,
-            "SubjectPart": "Your email subject",
-            "HtmlPart": "<html><body>Your HTML content</body></html>",
-            "TextPart": "Your plain text content",
-        }
-    )
-
-    def generate_test_password():
-        logging.info("generate_password")
-        return tempPassword
-
-    def _get_test_cognito_client():
-        try:
-            yield CognitoClient(
-                cognito_client,
-                ses_client,
-                generate_test_password,
-            )
-        finally:
-            pass
-
-    connection = engine.connect()
-    session = SessionTesting(bind=connection)
-
-    def _get_test_db():
-        try:
-            yield session
-        finally:
-            session.close()
-
-    # Override the clients dependencies with the mock implementations
-    app.dependency_overrides[get_cognito_client] = _get_test_cognito_client
-    app.dependency_overrides[get_db] = _get_test_db
-
-    with TestClient(app) as client:
-        yield client
-        connection.close()
-
+from tests.test_client import app, client  # isort:skip # noqa
+from tests.test_client import mock_cognito_client, mock_ses_client  # isort:skip # noqa
 
 data = {"email": "test@example.com", "name": "Test User", "type": UserType.FI.value}
 
 
-def test_create_user(client):
+def test_create_user(client):  # isort:skip # noqa
     response = client.post("/users", json=data)
     assert response.status_code == status.HTTP_200_OK
 
@@ -137,7 +19,7 @@ def test_create_user(client):
     assert response.status_code == status.HTTP_200_OK
 
 
-def test_duplicate_user(client):
+def test_duplicate_user(client):  # isort:skip # noqa
     response = client.post("/users", json=data)
     assert response.status_code == status.HTTP_200_OK
     # duplicate user
@@ -145,14 +27,14 @@ def test_duplicate_user(client):
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_login(client):
+def test_login(client):  # isort:skip # noqa
     responseCreate = client.post("/users", json=data)
     assert responseCreate.status_code == status.HTTP_200_OK
 
     setupPasswordPayload = {
         "username": data["email"],
-        "temp_password": tempPassword,
-        "password": tempPassword,
+        "temp_password": test_client.tempPassword,
+        "password": test_client.tempPassword,
     }
     responseSetupPassword = client.put(
         "/users/change-password", json=setupPasswordPayload
@@ -160,7 +42,7 @@ def test_login(client):
     logging.info(responseSetupPassword.json())
     assert responseSetupPassword.status_code == status.HTTP_200_OK
 
-    loginPayload = {"username": data["email"], "password": tempPassword}
+    loginPayload = {"username": data["email"], "password": test_client.tempPassword}
     responseLogin = client.post("/users/login", json=loginPayload)
     logging.info(responseLogin.json())
 
