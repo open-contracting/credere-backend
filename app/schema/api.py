c0db2ac0e2792981788d982d778fdf235c59f382
Nahuel Hernández <jnahuelhernandez@gmail.com>
2023-07-06 12:21:28 -0400

    fix: pre-commit

--- app/schema/api.py
+++ app/schema/api.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from decimal import Decimal
-from typing import List, Optional
 from enum import Enum
+from typing import List, Optional
 
 from pydantic import BaseModel
 
