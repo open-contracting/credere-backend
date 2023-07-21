d0b02424d2dc29296b5a242d9ae47735a708b9c1
adrian <adrian.martinez@willdom.com>
2023-07-21 17:38:38 -0400

    adding requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="Your FastAPI App",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=["sphinx_autodoc_typehints", "sphinx.ext.autodoc"],
-)
+setup(name="Your FastAPI App", version="0.1", packages=find_packages())
