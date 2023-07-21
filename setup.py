81ace5cdba479b0a27ed22fff08a846e69bc25ea
adrian <adrian.martinez@willdom.com>
2023-07-21 17:21:14 -0400

    added autodoc to setup

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="Your FastAPI App",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "sphinx_autodoc_typehints",
+        "sphinx.ext.autodoc"
+        # Any other dependencies
+    ],
 )
