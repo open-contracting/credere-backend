b2f390e58f41e5238e2f1f019a5975d69359d8e4
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2023-09-07 18:19:48 -0400

    fix: run alembic merge heads

---
+++ migrations/versions/63b0daed7f08_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 63b0daed7f08
+Revises: 03e96ecd4e99, 22acf38240bd
+Create Date: 2023-09-07 18:19:33.477272
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel # added
+
+
+# revision identifiers, used by Alembic.
+revision = '63b0daed7f08'
+down_revision = ('03e96ecd4e99', '22acf38240bd')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
