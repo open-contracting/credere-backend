7ceedfd70cf3a9807ec9e3f6975c8ac21c23c936
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-10-13 19:15:17 -0400

    test: Run test_log_level separately, as app_settings is initialized once

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,12 @@
 import logging
+import os
+
+import pytest
 
 
+@pytest.mark.skipif(
+    not os.getenv("TEST_SETTINGS"), reason="settings tests must be run separately"
+)
 def test_log_level(monkeypatch, capsys):
     monkeypatch.setenv("LOG_LEVEL", "WARNING")
 
@@ -12,4 +18,4 @@ def test_log_level(monkeypatch, capsys):
     captured = capsys.readouterr()
 
     assert captured.out == ""
-    assert captured.err[23:] == " WARNING [tests.test_settings:11] b\n"
+    assert captured.err[23:].endswith("] b\n")
