5881d456257bde023fc6c4c5d6bbfd2fa4e7b01d
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2023-12-11 16:52:02 -0300

    Apply suggestions from code review

--- app/routers/applications.py
+++ app/routers/applications.py
@@ -239,26 +239,22 @@ async def verify_data_field(
     with transaction_session(session):
         # Update a specific field in the application's `secop_data_verification` attribute.
         payload_dict = {key: value for key, value in payload.dict().items() if value is not None}
-        key, value = next(iter(payload_dict.items()), (None, None))
-        if not key:
-            logger.error("Trying to update an empty key")
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Cannot update a field without a name",
+        try:
+            key, value = next(iter(payload_dict.items()), (None, None))
+            verified_data = application.secop_data_verification.copy()
+            verified_data[key] = value
+            application.secop_data_verification = verified_data.copy()
+
+            models.ApplicationAction.create(
+                session,
+                type=models.ApplicationActionType.DATA_VALIDATION_UPDATE,
+                data=jsonable_encoder(payload, exclude_unset=True),
+                application_id=application.id,
+                user_id=user.id,
             )
-        verified_data = application.secop_data_verification.copy()
-        verified_data[key] = value
-        application.secop_data_verification = verified_data.copy()
-
-        models.ApplicationAction.create(
-            session,
-            type=models.ApplicationActionType.DATA_VALIDATION_UPDATE,
-            data=jsonable_encoder(payload, exclude_unset=True),
-            application_id=application.id,
-            user_id=user.id,
-        )
-
-        return application
+            return application
+        except StopIteration:
+            return application
 
 
 @router.put(
