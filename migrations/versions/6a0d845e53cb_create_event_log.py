cb53478f8c82388172406a29fd86ef4cfee8b4ba
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-04-03 09:19:00 -0400

    fix event log table migration

--- migrations/versions/6a0d845e53cb_create_event_log.py
+++ migrations/versions/6a0d845e53cb_create_event_log.py
@@ -1,18 +1,17 @@
 """create event log
 
-Revision ID: 808d2be1c329
+Revision ID: 6a0d845e53cb
 Revises: e4389132baa5
-Create Date: 2024-04-01 15:00:27.301963
+Create Date: 2024-04-03 09:18:14.453005
 
 """
-
 from alembic import op
 import sqlalchemy as sa
 import sqlmodel  # added
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = "808d2be1c329"
+revision = "6a0d845e53cb"
 down_revision = "e4389132baa5"
 branch_labels = None
 depends_on = None
@@ -23,9 +22,10 @@ def upgrade() -> None:
         "event_log",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("category", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
-        sa.Column("data", postgresql.JSON(astext_type=sa.Text()), nullable=True),
-        sa.Column("url", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
         sa.Column("message", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.Column("url", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.Column("data", postgresql.JSON(astext_type=sa.Text()), nullable=False),
+        sa.Column("traceback", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
         sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.text("now()"), nullable=False),
         sa.PrimaryKeyConstraint("id"),
     )
