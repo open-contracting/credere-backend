open-contracting/credere-backend

commit 9b08c6c77ca5b05e65eb1dca459d4bc417e3a78d

fix: logout

app/routers/users.py
@@ -196,17 +196,14 @@ def login_mfa(
     response_model=ApiSchema.ResponseBase,
 )
 def logout(
-    Authorization: str = Header(None),
+    authorization: str = Header(None),
     client: CognitoClient = Depends(get_cognito_client),
 ):
     try:
-        client.logout_user(Authorization)
+        access_token = authorization.split(" ")[1]
+        client.logout_user(access_token)
     except ClientError as e:
         logging.error(e)
-        # raise HTTPException(
-        #     status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        #     detail="There was an error trying to logout",
-        # )
 
     return ApiSchema.ResponseBase(detail="User logged out successfully")
 
@@ -219,7 +216,7 @@ def me(
     usernameFromToken: str = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    user = db.query(User).filter(User.email == usernameFromToken).first()
+    user = db.query(User).filter(User.external_id == usernameFromToken).first()
     return ApiSchema.UserResponse(user=user)
 
 

commit cfae81e68c2cd128c96df1098e4136a195c813e6

fix: Fix bug introduced in 9b08c6c, where logging out from expired session causes IndexError, closes #340

app/routers/users.py
@@ -199,24 +199,20 @@ def login(
     "/users/logout",
 )
 def logout(
-    authorization: str = Header(None),
+    authorization: str | None = Header(None),
     client: CognitoClient = Depends(dependencies.get_cognito_client),
 ) -> serializers.ResponseBase:
     """
     Logout the user by invalidating the access token.
 
-    This endpoint logs out the user by invalidating the provided access token.
-    It extracts the access token from the Authorization header,
-    invalidates the token using the Cognito client, and returns a response indicating
-    successful logout.
-
-    :param authorization: The Authorization header containing the access token.
+    :param authorization: The Authorization header, like "Bearer ACCESS_TOKEN".
     :return: The response indicating successful logout.
     """
-    try:
-        client.logout_user(authorization.split(" ")[1])
-    except ClientError as e:
-        logger.exception(e)
+    if authorization is not None:
+        try:
+            client.logout_user(authorization.split(" ")[1])
+        except ClientError as e:
+            logger.exception(e)
 
     return serializers.ResponseBase(detail="User logged out successfully")
 