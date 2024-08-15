92c5b42bc7fe96dcc8cce7451cb7180724f3a624
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2024-08-15 08:35:01 -0400

    fixes #348

--- app/mail.py
+++ app/mail.py
@@ -327,7 +327,7 @@ def send_upload_contract_confirmation(ses: SESClient, application: Application) 
                 "BUYER_NAME": application.award.buyer_name,
             },
         ),
-        to_msme=False,
+        to_msme=True,
     )
 
 
