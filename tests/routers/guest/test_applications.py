105e1fca4ccd86e7912211cc67582f5ad19db399
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-08-16 22:25:06 -0400

    test: Re-organize test files

---
+++ tests/routers/guest/test_applications.py
@@ -0,0 +1,85 @@
+from datetime import datetime, timedelta
+
+from fastapi import status
+
+from app import models
+from tests import assert_ok
+
+
+def test_application_declined(client, pending_application):
+    response = client.post(
+        "/applications/decline", json={"uuid": "123-456-789", "decline_this": False, "decline_all": True}
+    )
+
+    assert_ok(response)
+    assert response.json()["application"]["status"] == models.ApplicationStatus.DECLINED
+    assert response.json()["borrower"]["status"] == models.BorrowerStatus.DECLINE_OPPORTUNITIES
+
+    response = client.post("/applications/access-scheme", json={"uuid": "123-456-789"})
+
+    assert response.status_code == status.HTTP_409_CONFLICT
+    assert response.json() == {"detail": "Application status should not be DECLINED"}
+
+
+def test_application_rollback_declined(client, session, declined_application):
+    declined_application.borrower.status = models.BorrowerStatus.DECLINE_OPPORTUNITIES
+    session.commit()
+
+    response = client.post("/applications/rollback-decline", json={"uuid": "123-456-789"})
+
+    assert_ok(response)
+    assert response.json()["application"]["status"] == models.ApplicationStatus.PENDING
+    assert response.json()["borrower"]["status"] == models.BorrowerStatus.ACTIVE
+
+    response = client.post("/applications/rollback-decline", json={"uuid": "123-456-789"})
+
+    assert response.status_code == status.HTTP_409_CONFLICT
+    assert response.json() == {"detail": "Application status should not be PENDING"}
+
+
+def test_application_declined_feedback(client, declined_application):
+    declined_feedback_payload = {
+        "uuid": "123-456-789",
+        "dont_need_access_credit": True,
+        "already_have_acredit": False,
+        "preffer_to_go_to_bank": False,
+        "dont_want_access_credit": False,
+        "other": False,
+        "other_comments": "comments",
+    }
+
+    response = client.post("/applications/decline-feedback", json=declined_feedback_payload)
+
+    assert_ok(response)
+    assert response.json()["application"]["status"] == models.ApplicationStatus.DECLINED
+    declined_feedback_payload.pop("uuid")
+    assert response.json()["application"]["borrower_declined_preferences_data"] == declined_feedback_payload
+
+
+def test_access_expired_application(client, session, pending_application):
+    pending_application.expired_at = datetime.now(pending_application.created_at.tzinfo) - timedelta(days=+1)
+    session.commit()
+
+    # borrower tries to access expired application
+    response = client.get("/applications/uuid/123-456-789")
+
+    assert response.status_code == status.HTTP_409_CONFLICT
+    assert response.json() == {"detail": "Application expired"}
+
+
+def test_rollback_credit_product(client, accepted_application):
+    response = client.post(
+        "/applications/rollback-select-credit-product",
+        json={
+            "uuid": "123-456-789",
+            "borrower_size": models.BorrowerSize.SMALL,
+            "amount_requested": 10000,
+            "sector": "adminstration",
+            "credit_product_id": accepted_application.credit_product_id,
+        },
+    )
+
+    assert_ok(response)
+    assert response.json()["application"]["uuid"] == "123-456-789"
+    assert response.json()["application"]["credit_product_id"] is None
+    assert response.json()["application"]["borrower_credit_product_selected_at"] is None
--- tests/routers/test_applications.py
+++ tests/routers/test_applications.py
@@ -1,6 +1,5 @@
 import os
 from contextlib import contextmanager
-from datetime import datetime, timedelta
 from unittest.mock import patch
 
 from fastapi import status
@@ -34,35 +33,6 @@ def test_reject_application(client, session, lender_header, pending_application,
 
     response = client.post("/applications/find-alternative-credit-option", json=application_uuid_payload)
     assert_ok(response)
-
-
-def test_rollback_credit_product(client, accepted_application):
-    response = client.post(
-        "/applications/rollback-select-credit-product",
-        json={
-            "uuid": "123-456-789",
-            "borrower_size": models.BorrowerSize.SMALL,
-            "amount_requested": 10000,
-            "sector": "adminstration",
-            "credit_product_id": accepted_application.credit_product_id,
-        },
-    )
-
-    assert_ok(response)
-    assert response.json()["application"]["uuid"] == "123-456-789"
-    assert response.json()["application"]["credit_product_id"] is None
-    assert response.json()["application"]["borrower_credit_product_selected_at"] is None
-
-
-def test_access_expired_application(client, session, pending_application):
-    pending_application.expired_at = datetime.now(pending_application.created_at.tzinfo) - timedelta(days=+1)
-    session.commit()
-
-    # borrower tries to access expired application
-    response = client.get("/applications/uuid/123-456-789")
-
-    assert response.status_code == status.HTTP_409_CONFLICT
-    assert response.json() == {"detail": "Application expired"}
 
 
 def test_approve_application_cycle(
@@ -389,53 +359,3 @@ def test_get_applications(client, session, admin_header, lender_header, pending_
     response = client.post("/applications/access-scheme", json={"uuid": "123-456"})
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "Application not found"}
-
-
-def test_application_declined(client, pending_application):
-    response = client.post(
-        "/applications/decline", json={"uuid": "123-456-789", "decline_this": False, "decline_all": True}
-    )
-
-    assert_ok(response)
-    assert response.json()["application"]["status"] == models.ApplicationStatus.DECLINED
-    assert response.json()["borrower"]["status"] == models.BorrowerStatus.DECLINE_OPPORTUNITIES
-
-    response = client.post("/applications/access-scheme", json={"uuid": "123-456-789"})
-
-    assert response.status_code == status.HTTP_409_CONFLICT
-    assert response.json() == {"detail": "Application status should not be DECLINED"}
-
-
-def test_application_rollback_declined(client, session, declined_application):
-    declined_application.borrower.status = models.BorrowerStatus.DECLINE_OPPORTUNITIES
-    session.commit()
-
-    response = client.post("/applications/rollback-decline", json={"uuid": "123-456-789"})
-
-    assert_ok(response)
-    assert response.json()["application"]["status"] == models.ApplicationStatus.PENDING
-    assert response.json()["borrower"]["status"] == models.BorrowerStatus.ACTIVE
-
-    response = client.post("/applications/rollback-decline", json={"uuid": "123-456-789"})
-
-    assert response.status_code == status.HTTP_409_CONFLICT
-    assert response.json() == {"detail": "Application status should not be PENDING"}
-
-
-def test_application_declined_feedback(client, declined_application):
-    declined_feedback_payload = {
-        "uuid": "123-456-789",
-        "dont_need_access_credit": True,
-        "already_have_acredit": False,
-        "preffer_to_go_to_bank": False,
-        "dont_want_access_credit": False,
-        "other": False,
-        "other_comments": "comments",
-    }
-
-    response = client.post("/applications/decline-feedback", json=declined_feedback_payload)
-
-    assert_ok(response)
-    assert response.json()["application"]["status"] == models.ApplicationStatus.DECLINED
-    declined_feedback_payload.pop("uuid")
-    assert response.json()["application"]["borrower_declined_preferences_data"] == declined_feedback_payload
