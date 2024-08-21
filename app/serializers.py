b9b895267b68a6a2b79ae399b35976eae00a2304
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-08-20 22:19:41 -0400

    docs: Document frontend schema for fields of serializers

--- app/serializers.py
+++ app/serializers.py
@@ -1,3 +1,5 @@
+# Credere frontend schema are in comments.
+
 from typing import Any
 
 from pydantic import BaseModel, Field
@@ -6,19 +8,20 @@ from app import models
 
 
 class ApplicationResponse(BaseModel):
-    application: models.ApplicationRead
-    borrower: models.Borrower
-    award: models.Award
-    lender: models.Lender | None = None
-    documents: list[models.BorrowerDocumentBase] = Field(default_factory=list)
-    creditProduct: models.CreditProduct | None = None
+    application: models.ApplicationRead  # IApplication
+    borrower: models.Borrower  # IBorrower
+    award: models.Award  # IAward
+    lender: models.Lender | None = None  # ILender
+    documents: list[models.BorrowerDocumentBase] = Field(default_factory=list)  # IBorrowerDocument
+    creditProduct: models.CreditProduct | None = None  # ICreditProduct
 
 
 class CreditProductListResponse(BaseModel):
-    loans: list[models.CreditProductWithLender]
-    credit_lines: list[models.CreditProductWithLender]
+    loans: list[models.CreditProductWithLender]  # ICreditProduct
+    credit_lines: list[models.CreditProductWithLender]  # ICreditProduct
 
 
+# Abtract
 class BasePagination(BaseModel):
     count: int
     page: int
@@ -26,15 +29,15 @@ class BasePagination(BaseModel):
 
 
 class ApplicationListResponse(BasePagination):
-    items: list[models.ApplicationWithRelations]
+    items: list[models.ApplicationWithRelations]  # IApplication
 
 
 class LenderListResponse(BasePagination):
-    items: list[models.Lender]
+    items: list[models.Lender]  # ILender
 
 
 class UserListResponse(BasePagination):
-    items: list[models.UserWithLender]
+    items: list[models.UserWithLender]  # IUser
 
 
 class ResponseBase(BaseModel):
@@ -48,7 +51,7 @@ class ChangePasswordResponse(ResponseBase):
 
 
 class UserResponse(BaseModel):
-    user: models.User
+    user: models.User  # IUser
 
 
 class LoginResponse(UserResponse):
@@ -57,10 +60,10 @@ class LoginResponse(UserResponse):
 
 
 class StatisticResponse(BaseModel):
-    statistics_kpis: dict[Any, Any]
+    statistics_kpis: dict[Any, Any]  # StatisticsKpis
 
 
-class StatisticData(BaseModel):
+class StatisticData(BaseModel):  # ChartData
     name: str
     value: int
 
@@ -69,4 +72,4 @@ class StatisticData(BaseModel):
 
 
 class StatisticOptInResponse(BaseModel):
-    opt_in_stat: dict[Any, Any]
+    opt_in_stat: dict[Any, Any]  # OptInStat
