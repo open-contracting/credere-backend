bfa11bf584e2b47eb60f5332668082c2243154dd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-12-16 16:10:17 -0500

    build: Run npx storybook@9 upgrade
    No change after running: npx storybook@latest migrate csf-2-to-3 --glob="**/*.stories.tsx" --parser=tsx
    
    https://storybook.js.org/docs/releases/migration-guide-from-older-version
    https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#dropped-support-for-legacy-packages

--- frontend/src/stories/faq/FAQ.stories.tsx
+++ frontend/src/stories/faq/FAQ.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { FAQContainer } from "./FAQContainer";
 import { FAQSection } from "./FAQSection";
