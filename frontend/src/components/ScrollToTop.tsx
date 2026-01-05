7e7d0ee00a0b955f4b4a76058de2efd370f9765a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-01-05 16:15:26 -0500

    chore: pathname is not used

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -1,16 +1,13 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router";
 
 export function ScrollToTop() {
-  const { pathname } = useLocation();
-
   useEffect(() => {
     // "document.documentElement.scrollTo" is the magic for React Router Dom v6
     document.documentElement.scrollTo({
       top: 0,
       left: 0,
     });
-  }, [pathname]);
+  }, []);
 
   return null;
 }
